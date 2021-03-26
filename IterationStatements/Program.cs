1d35caa80399b7d2fc9c2e09c2a6c45360f79b92
IVProb <jnoojinjr@gmail.com>
2021-03-26 13:36:55 -0500

    for loops 15x

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -7,11 +7,91 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-                //TODO - Read each comment and complete its instruction
-                // like the example below
+            // for loop written 15x
 
-                //Create a List called "numbers" - DONE!
-                var numbers = new List<int>();
+            // 1)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 2)
+            for ( int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 3)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 4)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 5)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{}");
+            }
+            // 6)
+            for (int i =0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 7)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 8)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i++}");
+            }
+            // 9)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 10)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 11)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 12)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 13)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 14)
+            for ( int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            // 15)
+            for ( int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+       
+
+
+            //TODO - Read each comment and complete its instruction
+            // like the example below
+
+            //Create a List called "numbers" - DONE!
+            var numbers = new List<int>();
             
             //-----START HERE------------------------------------------
             //Create a variable of type int and name it num
